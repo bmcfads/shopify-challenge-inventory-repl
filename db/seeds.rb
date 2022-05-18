07f3e8dedd7473b83fee06655f4c90979cdc069b
BMCFADS <101163132+bmcfads@users.noreply.github.com>
2022-05-18 06:10:58 +0000

    Adds database seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,95 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Warhouses
+# ----------------------------------
+Warehouse.create(
+  {
+    name: 'APAC Distribution Centre',
+    location: 'Hong Kong'
+  }
+)
+
+Warehouse.create(
+  {
+    name: 'EMEA Distribution Centre',
+    location: 'Amsterdam'
+  }
+)
+
+Warehouse.create(
+  {
+    name: 'LATAM Distribution Centre',
+    location: 'Panama City'
+  }
+)
+
+Warehouse.create(
+  {
+    name: 'NA Distribution Centre',
+    location: 'Vancouver'
+  }
+)
+
+# Items
+# ----------------------------------
+Item.create(
+  {
+    sku: 'LNGB34WDBLK0',
+    name: 'Longboard',
+    description: 'Globe Big Blazer 32" complete cruiser.'
+  }
+)
+
+Item.create(
+  {
+    sku: 'SKIQ98L37509',
+    name: 'Skis',
+    description: 'Salomon Q-98s, 180 cm.'
+  }
+)
+
+Item.create(
+  {
+    sku: 'THURSOWW0132',
+    name: 'Paddleboard',
+    description: 'Thurso Surf Waterwalker 132.'
+  }
+)
+
+Item.create(
+  {
+    sku: 'STMPJMPEVOP7',
+    name: 'Mountain Bike',
+    description: 'Specialized Stumpjumper EVO Pro.'
+  }
+)
+
+Item.create(
+  {
+    sku: 'SALADVSKN5BM',
+    name: 'Running Vest',
+    description: 'Salomon ADV Skin 5, black, medium.'
+  }
+)
+
+Item.create(
+  {
+    sku: 'GPHERO10BLK1',
+    name: 'Camera',
+    description: 'GoPro HERO10, black, with subscription.'
+  }
+)
+
+Item.create(
+  {
+    sku: 'VAN085GRY005',
+    name: 'Shoes',
+    description: 'Vans classic shoes, 8.5, grey.'
+  }
+)
+
+Item.create(
+  {
+    sku: 'NALBTLGRN001',
+    name: 'Water Bottle',
+    description: 'Nalgene, green, 1 L.'
+  }
+)
