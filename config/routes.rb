225fae48593a0b34e95a815986b7085ae249dd29
BMCFADS <101163132+bmcfads@users.noreply.github.com>
2022-05-18 06:10:23 +0000

    Updates routes configuration.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  resources :inventories
+  root 'warehouses#index'
   resources :warehouses
   resources :items
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :inventories
 end
