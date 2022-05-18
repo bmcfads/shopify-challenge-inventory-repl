bd1df12a3392830f0e9034a54dc9c51862611e4f
BMCFADS <101163132+bmcfads@users.noreply.github.com>
2022-05-18 05:58:36 +0000

    Adds Inventory model validations.

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,4 +1,7 @@
 class Inventory < ApplicationRecord
   belongs_to :warehouse
   belongs_to :item
+
+  validates :item_id, uniqueness: { scope: :warehouse_id, message: 'already exists in inventory for this warehouse. Edit inventory record instead.' }
+  validates :quantity, comparison: { greater_than_or_equal_to: 0 }
 end
