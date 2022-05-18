514f6109c73c86312e851f64e5a3b72c7eceaf7d
BMCFADS <101163132+bmcfads@users.noreply.github.com>
2022-05-18 05:59:20 +0000

    Adds Item model validations.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ApplicationRecord
   has_many :inventories, dependent: :destroy
   has_many :warehouses, through: :inventories
+
+  validates :name, presence: true, length: { maximum: 35 }
+  validates :sku, presence: true, uniqueness: true, length: { maximum: 12 }
+  validates :description, length: { maximum: 140 }
 end
