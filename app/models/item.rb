6cff92e29311a4001090b4755f2b6766db389eac
BMCFADS <101163132+bmcfads@users.noreply.github.com>
2022-05-18 05:56:37 +0000

    Adds model associations.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,4 @@
 class Item < ApplicationRecord
+  has_many :inventories, dependent: :destroy
+  has_many :warehouses, through: :inventories
 end
--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -1,2 +1,4 @@
 class Warehouse < ApplicationRecord
+  has_many :inventories, dependent: :destroy
+  has_many :items, through: :inventories
 end
