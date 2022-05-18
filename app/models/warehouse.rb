084bffd52daa46eb4db0a92561f2fbbfc964b632
BMCFADS <101163132+bmcfads@users.noreply.github.com>
2022-05-18 06:00:17 +0000

    Adds Warehouse model validations.

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -1,4 +1,7 @@
 class Warehouse < ApplicationRecord
   has_many :inventories, dependent: :destroy
   has_many :items, through: :inventories
+
+  validates :name, presence: true, length: { maximum: 35 }
+  validates :location, presence: true, length: { maximum: 35 }
 end
